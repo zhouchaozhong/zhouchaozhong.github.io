0c60d38c4845bbdb943c5c9635186732958bc1b1
charles <charles@gmail.com>
2024-11-07 18:08:52 +0800

    个人主页

---
+++ assets/Project--wt78Cc3.js
@@ -0,0 +1,58 @@
+import{u as d}from"./common-B4zVUdu9.js";import{d as a,r as c,b as l,c as i,a as e,F as p,f as b,o as n,t as s,_ as j}from"./index-WWSgSjph.js";const u=[{id:1,projectName:"湘雅二医院绩效考核系统",date:"2024.06 - 2024.08",link:"",linkDesc:"",order:1,enable:!0,description:`
+            该项目是公司为湘雅二医院内部绩效考核而开发的系统，主要功能包括科室管理，岗位管理，员工管理，绩效标准管理，绩效考核报表，共享知识库等功能。该系统采用SpringBoot开发，
+            使用redis做缓存，使用mysql 作为数据库，使用的SpringSecurity开发的权限系统，文件系统采用的是MINIO ，使用VUE框架开发来开发的web管理页面。
+        `,jobContent:`
+            负责该系统的数据库表设计，负责系统基本功能划分设计和技术选型，负责系统底层工具库开发，同时负责跟客户沟通相关业务需求，也负责了绩效标准管理，绩效考核报表的后端功能开发，
+            以及部分前端功能的开发。
+        `},{id:2,projectName:"湖南洋马OA管理系统",date:"2024.02 - 2024.05",link:"",linkDesc:"",order:2,enable:!0,description:`
+            该项目是公司为了管理内部事务而开发的OA系统，，主要功能包括项目管理， 采购管理，询价管理，财务管理，资产管理，考勤管理，知识库。该系统采用SpringBoot开发，
+            使用redis做缓存，使用mysql作为数据库，文件系统采用的是MINIO ，采用activiti作为流程引擎来开发工作流相关功能，使用JNI来跟考勤机通信，完成考勤打卡功能，
+            该系统采用的是前后端分离架构开发，使用VUE框架开发Web前端
+        `,jobContent:`
+            负责该系统的数据库表设计，负责系统基本功能划分设计和技术选型，负责系统底层工具库开发，负责各子功能模块工作流和审批功能开发，负责开发考勤管理功能。
+        `},{id:3,projectName:"港益管理系统",date:"2023.09 - 2023.12",link:"",linkDesc:"",order:3,enable:!0,description:`
+            该项目是公司为湖南省各级港口开发的内部管理系统，主要是用来管理港口内各种设施资源及事务，主要功能包括船舶管理，泊位管理，洗舱作业，危货作业，航线管理，公共知识库，
+            轨道管理，班前会议，吞吐量监控，作业人员管理，作业资格证管理等。 该系统采用SpringBoot开发，使用redis做缓存，使用mysql作为数据库，文件系统采用的是MINIO ，
+            该系统采用的前后端分离架构开发，使用VUE框架开发Web前端。
+        `,jobContent:`
+            负责该系统的数据库表设计，负责系统基本功能划分设计和技术选型，负责系统常用功能工具库开发和作业管理功能的开发。
+        `},{id:4,projectName:"湖南省政务公开评估平台",date:"2023.05 - 2023.09",link:"",linkDesc:"",order:4,enable:!0,description:`
+            该项目是公司为湖南省政府开发的政务公开评估平台，主要功能包括评估指标权重分配，年度分析，单位层级分析，指标得分率分析，指标雷达图，地区单位分析， 公开评估，档案管理等。
+            该系统采用SpringBoot开发，使用redis做缓存，使用mysql作为数据库，文件系统采用的是MINIO ，该系统采用的前后端分离架构开发，使用VUE框架开发Web前端。
+        `,jobContent:`
+            负责系统迭代的数据库表设计，封装系统基础功能，包括用户权限管理，菜单 权限分配，公开评估系统开发，系统部署与交付。
+        `},{id:5,projectName:"湖南港口监管信息系统",date:"2022.12-2023.03",link:"",linkDesc:"",order:5,enable:!0,description:`
+            该项目是公司为湖南水运集团开发的港口监管系统，主要功能包括港口管理， 港区管理，港口经营人管理，经营证照管理，船舶管理，泊位管理，船舶作业管理等。 
+            该系统采用SpringBoot开发，使用redis做缓存，使用mysql作为数据库，使用Activiti作为流程引擎来开发各种审批流程，文件系统采用的是MINIO ，
+            采用Websocket和MQTT协议与港口内部分IOT监测设备通信，该系统采用的前后端分离架构开发，使用VUE框架开发Web前端，uniapp开发的小程序端。
+        `,jobContent:`
+            负责系统迭代的数据库表设计，参与系统迭代的技术选型，流程引擎基础功能 封装，作业管理功能后端模块的开发。同时也参与部分web前端功能和uniapp小程序端功能的开发。
+        `},{id:6,projectName:"创智WMS仓储管理系统",date:"2022.06 - 2022.10",link:"",linkDesc:"",order:6,enable:!0,description:`
+            该项目是公司开发的WMS仓储管理系统，主要功能包括商品管理，包材管理，仓库管理，容器管理，库区，库架，库位管理，入库管理，出库管理，库存管理等。
+            该系统采用SpringBoot开发，使用redis做缓存，使用rabbitmq来做消息中间件，使用mysql作为数据库，使用websocket来跟手持终端通信，
+            手持终端识别 RFID 标签信息， 自动录入入库信息，出库信息，上架信息。
+        `,jobContent:`
+            商品管理，包材管理，库存管理模块的后端开发，以及系统的部署和维护。
+        `},{id:7,projectName:"鲁南高铁BIM工程信息管理系统",date:"2021.11 - 2022.03",link:"",linkDesc:"",order:7,enable:!0,description:`
+            该项目是公司和中铁集团合作的山东鲁南高铁项目的 BIM 工程信息管理系统， 主要功能包括质量管理，征拆管理，设计管理，物资计划管理，工程进度统计，供货管理， 工程验收等。
+            该系统采用微服务架构，使用SpringCloud开发，使用Nacos作为注册中心和配置中心，SpringCloud Gateway作为网关，ribbon 做负载均衡，Hystrix做熔断器，
+            MyBatisPlus来做持久层，数据库采用一主二从读写分离的架构，使用的shardingsphere实现的分表和读写分离，redis做缓存，SpringSecurity做权限控制， 
+            使用rabbitmq来做消息中间件，实现消息通知以及延时任务处理，使用activiti来做工作流引擎，实现系统中的计划流程的审批。
+        `,jobContent:`
+            主要负责物资计划管理，分部分项工程计划管理模块的后端开发，以及系统的部署与维护，该项目使用docker容器化部署，采用了docker-compose 工具实现了服务的批量化部署。
+        `},{id:8,projectName:"1119工程建设管理平台",date:"2020.05 - 2020.09",link:"",linkDesc:"",order:8,enable:!0,description:`
+            1119 项目是公司专为工程建设管理而研发的产品，已对接的工程有南昌西二环 和东陵高速，该产品用于数字化管理工程项目，如单项工程、工程量清单、标段、台账等，
+            主要功能有合同管理、分包管理、计量支付、变更管理、质量控制、安全控制、进度管理、协同办公、劳务管理以及流程审批。项目采用 SpringBoot+MyBatisPlus单体架构，
+            使用redis做缓存，数据库使用MySQL ，使用 go-fastdfs 文件系统。
+        `,jobContent:`
+            本人在项目中承担了基础框架改造，开发了单项工程、标段、报表模板、计量支付和变更管理等模块。项目中存在大量树形结构的数据，通过合理的表字段设计和SQL优化，达到了较为满意的查询效率。
+        `},{id:9,projectName:"脉极酷商城",date:"2018.06 - 2018.10",link:"",linkDesc:"",order:9,enable:!0,description:`
+            该项目是主打销售运动品牌的商城，是公司跟深圳一家销售运动器材和服装的实体公司合作的项目，主要是帮助该公司发展互联网业务，公司从其中进行分成。该项目最开始采用的是SpringBoot开发业务层，
+            MyBatis做持久层，短信业务采用的是阿里云的短信业务，前端静态资源，以及图片和文件存储服务用的是阿里云的OSS，使用的是单体架构，商城的搜索也是直接查询的数据库，后期由于商城用户量激增，
+            以及后期加入的团购秒杀模块，原来的的项目架构已经不满足需求，于是对项目整体的架构进行了升级，首先将商城的热点数据进行了缓存，经常访问的首页推荐数据，热卖专区，人气专区的数据都缓存在了 
+            Redis中，同时对redis做了主从的架构。MySQL也使用了一主二从的架构，同时商城的搜索也由原来的直接从数据库查询，改为从 ElasticSearch 中查询，同时主体的业务逻辑也从单机扩展为多机，
+            同时把秒杀，团购业务等分离处理，单独做了一个应用，由于业务的分开，引入了redis分布式session和基于Redis实现的分布式锁， 来解决多机session同步的问题以及多个程序同时操作一个资源发生的
+            数据冲突问题。 同时为了解决秒杀活动，数据库访问压力大的问题，引入了 RabbitMQ 来对流量进行削峰，把秒杀成功的订单放入消息队列，排队进行处理。
+        `,jobContent:`
+           主要负责商城的搜索模块、商品管理模块，营销活动模块的开发，同时也负责部分前端页面的开发，以及整个项目的数据库的设计
+        `}],m={class:"project-box"},k={class:"project-list"},_={class:"title"},S={class:"date"},N={class:"project-name"},v={class:"content project-desc"},g={class:"desc"},y={class:"content job-content"},M={class:"desc"},B=a({__name:"Project",setup(I){const r=c(u);return l(()=>{d()}),(C,t)=>(n(),i("div",m,[e("ul",k,[(n(!0),i(p,null,b(r.value,(o,D)=>(n(),i("li",{class:"project-item",key:o.id},[e("div",_,[e("div",S,s(o.date),1),e("div",N,s(o.projectName),1)]),e("div",v,[t[0]||(t[0]=e("div",{class:"label"},"项目描述：",-1)),e("div",g,s(o.description),1)]),e("div",y,[t[1]||(t[1]=e("div",{class:"label"},"个人职责：",-1)),e("div",M,s(o.jobContent),1)]),t[2]||(t[2]=e("div",{class:"divider"},null,-1))]))),128))])]))}}),q=j(B,[["__scopeId","data-v-03e43a31"]]);export{q as default};
